672089fb9cf86f9ef1b2173b553c87465c800624
Airat Khairullin <a1rat91@yandex.ru>
2024-11-14 15:19:12 +0300

    fix: corrected the selection of elements. Not deselecting elements when drag drop by scene

--- projects/ng-draw-flow/src/lib/directives/selectable-element/selectable-element.directive.ts
+++ projects/ng-draw-flow/src/lib/directives/selectable-element/selectable-element.directive.ts
@@ -11,19 +11,55 @@ import {
 export class SelectableElementDirective {
     private selected = false;
     private readonly el = inject(ElementRef);
+    private isDragging = false;
+    private startX: number | null = null;
+    private startY: number | null = null;
+    private readonly dragThreshold = 5; // Displacement threshold for drag detection
 
     @Output()
     protected readonly selectionChanged = new EventEmitter<boolean>();
 
-    @HostListener('document:mousedown', ['$event.target'])
-    protected onDocumentClick(targetElement: any): void {
-        const clickedInside = this.el.nativeElement.contains(targetElement);
+    @HostListener('document:mousedown', ['$event'])
+    protected onMouseDown(event: MouseEvent): void {
+        this.startX = event.clientX;
+        this.startY = event.clientY;
+        const targetElement = event.target as HTMLElement;
 
-        if (targetElement.dataset.notSelectable) {
+        this.isDragging = false;
+
+        if (targetElement.dataset.element !== 'scene') {
+            const clickedInside = this.el.nativeElement.contains(targetElement);
+
+            this.setSelected(clickedInside);
+        }
+    }
+
+    @HostListener('document:mousemove', ['$event'])
+    protected onMouseMove(event: MouseEvent): void {
+        if (this.startX !== null && this.startY !== null) {
+            const dx = event.clientX - this.startX;
+            const dy = event.clientY - this.startY;
+
+            // Check if the movement exceeds the dragThreshold threshold
+            if (Math.sqrt(dx * dx + dy * dy) > this.dragThreshold) {
+                this.isDragging = true;
+            }
+        }
+    }
+
+    @HostListener('document:mouseup', ['$event.target'])
+    protected onMouseUp(targetElement: any): void {
+        if (this.isDragging && targetElement.dataset.element === 'scene') {
+            // If it was a drag and drop across the scene, we do nothing
+            this.reset();
+
             return;
         }
 
+        const clickedInside = this.el.nativeElement.contains(targetElement);
+
         this.setSelected(clickedInside);
+        this.reset();
     }
 
     private setSelected(selected: boolean): void {
@@ -46,5 +82,11 @@ export class SelectableElementDirective {
 
     private deselectNode(): void {
         this.el.nativeElement.classList.remove('df-selected');
+    }
+
+    private reset(): void {
+        this.startX = null;
+        this.startY = null;
+        this.isDragging = false;
     }
 }
