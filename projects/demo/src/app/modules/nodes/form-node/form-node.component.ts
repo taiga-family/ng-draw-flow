b568a20dda84953cf10797b0bbf7f1a662fce876
Хайруллин Айрат Ильдусович <ay.i.khayrullin@tbank.ru>
2025-05-13 15:43:58 +0300

    feat: implement visual feedback for invalid nodes

--- projects/demo/src/app/modules/nodes/form-node/form-node.component.ts
+++ projects/demo/src/app/modules/nodes/form-node/form-node.component.ts
@@ -1,11 +1,17 @@
 import {CommonModule} from '@angular/common';
-import type {AfterViewInit} from '@angular/core';
-import {ChangeDetectionStrategy, Component, DestroyRef, inject} from '@angular/core';
+import type {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    DestroyRef,
+    inject,
+} from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {DfInputComponent, DfOutputComponent, DrawFlowBaseNode} from '@ng-draw-flow/core';
 import {TuiButton} from '@taiga-ui/core';
 import {TuiInputModule, TuiTextfieldControllerModule} from '@taiga-ui/legacy';
+import {distinctUntilChanged, merge} from 'rxjs';
 
 interface NodeForm {
     field1: FormGroup<NodeFormGroup>;
@@ -38,15 +44,14 @@ interface NodeFormGroup {
     },
 })
 export class FormNodeComponent extends DrawFlowBaseNode implements AfterViewInit {
-    public currentIndex = 1;
     public form = new FormGroup<NodeForm>({
         field1: new FormGroup<NodeFormGroup>({
             connectorId: new FormControl<string>('node-5-output-1'),
-            fieldValue: new FormControl<string>(''),
+            fieldValue: new FormControl<string>('', [Validators.required]),
         }),
         field2: new FormGroup<NodeFormGroup>({
             connectorId: new FormControl<string>('node-5-output-2'),
-            fieldValue: new FormControl<string>(''),
+            fieldValue: new FormControl<string>('', [Validators.required]),
         }),
     });
 
@@ -72,6 +77,16 @@ export class FormNodeComponent extends DrawFlowBaseNode implements AfterViewInit
             .subscribe((value) => {
                 this.model.value = value;
             });
+
+        merge(
+            ...Object.values(this.form.controls).map((control) =>
+                control.controls.fieldValue.statusChanges.pipe(distinctUntilChanged()),
+            ),
+        )
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe(() => {
+                this.checkFormValidity();
+            });
     }
 
     public add(): void {
@@ -84,5 +99,14 @@ export class FormNodeComponent extends DrawFlowBaseNode implements AfterViewInit
 
         // @ts-ignore
         this.form.addControl(newFieldKey, newField);
+    }
+
+    private checkFormValidity(): void {
+        this.invalid = Object.values(this.form.controls).some((fieldGroup) => {
+            const group = fieldGroup as FormGroup<NodeFormGroup>;
+            const fieldValue = group.controls.fieldValue;
+
+            return fieldValue.dirty && fieldValue.invalid;
+        });
     }
 }
