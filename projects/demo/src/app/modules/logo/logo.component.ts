563cf9ddc8e6124eb4361a8f32a5c86824799615
Tobias Hassebrock <tobias.hassebrock@gmail.com>
2025-05-31 12:31:57 +0200

    docs: remove comments

--- projects/demo/src/app/modules/logo/logo.component.ts
+++ projects/demo/src/app/modules/logo/logo.component.ts
@@ -1,5 +1,3 @@
-// import {TUI_IS_E2E} from '@taiga-ui/cdk';
-// import { isPlatformServer } from '@angular/common';
 import {HttpClient} from '@angular/common/http';
 import type {OnInit} from '@angular/core';
 import {
@@ -7,7 +5,7 @@ import {
     Component,
     DestroyRef,
     inject,
-    /* PLATFORM_ID,*/ signal,
+    signal,
 } from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {RouterLink} from '@angular/router';
@@ -27,17 +25,10 @@ import {environment} from '../../../environments/environment';
 })
 export class LogoComponent implements OnInit {
     protected readonly stars = signal('');
-    // private readonly isE2E = inject(TUI_IS_E2E);
-    // private readonly isServer = isPlatformServer(inject(PLATFORM_ID));
     private readonly destroyRef = inject(DestroyRef);
     private readonly http = inject(HttpClient);
 
     public ngOnInit(): void {
-        /* uncomment this block if you want to skip the GitHub stars fetch in server or e2e mode or dev mode
-        if (this.isServer || this.isE2E || !environment.production) {
-            return;
-        }*/
-
         this.http
             .get<Record<string, any>>(environment.github)
             .pipe(takeUntilDestroyed(this.destroyRef))
