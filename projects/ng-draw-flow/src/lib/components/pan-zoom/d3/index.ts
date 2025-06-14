7bdd6b6248a5613be0058f79b9d9fb42b13e23ea
Airat Khairullin <a1rat91@yandex.ru>
2025-06-14 23:00:04 +0300

    refactor: panzoom

--- projects/ng-draw-flow/src/lib/components/pan-zoom/d3/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './d3-zoom.directive';
-export * from './d3-zoom.service';
-export * from './zoom.interfaces';
--- projects/ng-draw-flow/src/lib/components/pan-zoom/d3/zoom.interfaces.ts
+++
@@ -1,6 +0,0 @@
-import type {DfPoint} from '../../../ng-draw-flow.interfaces';
-
-export interface DfZoomTransform {
-    readonly position: DfPoint;
-    readonly scale: number;
-}
