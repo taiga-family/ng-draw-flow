5331448184bb481f44a77fd9db97dca2fa0e85ce
Airat Khairullin <a1rat91@yandex.ru>
2025-08-26 22:15:14 +0300

    chore: documentation/connections page

--- projects/demo/src/pages/documentation/connectors/connectors.component.ts
+++ projects/demo/src/pages/documentation/connectors/connectors.component.ts
@@ -1,12 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 import {NgDrawFlowComponent} from '@ng-draw-flow/core';
 import {TuiAddonDoc} from '@taiga-ui/addon-doc';
+import {TuiLink} from '@taiga-ui/core';
 
 @Component({
     standalone: true,
     selector: 'app-connectors',
-    imports: [NgDrawFlowComponent, ReactiveFormsModule, TuiAddonDoc],
+    imports: [NgDrawFlowComponent, ReactiveFormsModule, RouterLink, TuiAddonDoc, TuiLink],
     templateUrl: './connectors.component.html',
     styleUrls: ['./connectors.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
