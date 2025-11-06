0354ca5918da66b494b86e31482e1e82ef6d6223
Airat Khairullin <a1rat91@yandex.ru>
2025-11-06 11:07:49 +0300

    chore(demo): small fix

--- projects/demo/src/pages/examples/editor/editor.component.ts
+++ projects/demo/src/pages/examples/editor/editor.component.ts
@@ -167,7 +167,7 @@ export default class EditorComponent implements OnInit {
     };
 
     public readonly scaleControl = new FormControl<number>(1, {nonNullable: true});
-    public fullscreen$ = new BehaviorSubject<boolean>(false);
+    public readonly fullscreen$ = new BehaviorSubject<boolean>(false);
     public counter = 0;
     public form = new FormControl<DfDataModel>(this.data, [
         dfCycleDetectionValidator(),
