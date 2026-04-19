d86ccbbc47d1c1793b0050b85281cff25a78bcf6
Airat Khairullin <a1rat91@yandex.ru>
2026-04-19 15:12:05 +0300

    chore(ng-draw-flow): migrate to output functions

--- projects/ng-draw-flow/src/lib/directives/drag-drop/drag-drop.directive.ts
+++ projects/ng-draw-flow/src/lib/directives/drag-drop/drag-drop.directive.ts
@@ -1,15 +1,16 @@
 import {Directive, ElementRef, inject} from '@angular/core';
+import {outputFromObservable} from '@angular/core/rxjs-interop';
 
 import {DragDropService} from './drag-drop.service';
 
 @Directive({
     standalone: true,
     selector: '[dfDragDrop]',
-    outputs: ['dfDragDrop'],
 })
 export class DragDropDirective {
     protected readonly elementRef: ElementRef<HTMLElement> = inject(ElementRef);
-    protected readonly dfDragDrop = inject(DragDropService).streamFor(
-        this.elementRef.nativeElement,
+    protected readonly dfDragDrop = outputFromObservable(
+        inject(DragDropService).streamFor(this.elementRef.nativeElement),
+        {alias: 'dfDragDrop'},
     );
 }
